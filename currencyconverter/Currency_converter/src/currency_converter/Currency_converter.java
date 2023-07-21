29a7fc99c63da795c6f8c6bdd3d83021f4f41f33
agumas-atnaf-10 <139708385+agumas-atnaf-10@users.noreply.github.com>
2023-07-22 00:27:34 +0530

    Add files via upload

---
+++ currencyconverter/Currency_converter/src/currency_converter/Currency_converter.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package currency_converter;
+
+import My_Currency_Converter.MyCurrencyConverter;
+
+/**
+ *
+ * @author ag
+ */
+public class Currency_converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MyCurrencyConverter m=new  MyCurrencyConverter();
+    }
+    
+}
